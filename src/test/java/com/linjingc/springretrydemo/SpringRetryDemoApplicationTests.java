f80a26cd0143431ace7dbb495be48ae7dd451c81
chenxiaocheng <583188551@qq.com>
2022-12-21 11:30:49 +0800

    Initial commit

---
+++ src/test/java/com/linjingc/springretrydemo/SpringRetryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.springretrydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRetryDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
